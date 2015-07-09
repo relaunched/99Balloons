6933d8b505dd1428ae0028f57d02e39e90d6d97b
Matthew Hurewitz <Matthew.Hurewitz@bestbuy.com>
2015-07-09 09:56:21 -0500

    Initial commit

---
+++ 99Balloons/RedBalloon.swift
@@ -0,0 +1,16 @@
+//
+//  99Balloon.swift
+//  99Balloons
+//
+//  Created by Hurewitz, Matthew on 7/8/15.
+//  Copyright (c) 2015 Hurewitz, Matthew. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct RedBalloon {
+    var number = 0
+    var image = UIImage(named: "")
+    
+}
--- 99Balloons/ViewController.swift
+++ 99Balloons/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    var myBalloons:[RedBalloon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createBalloons()
+        
+       
+
+//         Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextBalloonButton(sender: UIBarButtonItem) {
+////        
+        var randomIndex = Int(arc4random_uniform(UInt32(100)))
+//        println("\(myBalloons[randomIndex].image)")
+        self.myImageView.image = myBalloons[randomIndex].image
+        self.balloonLabel.text = "Red Balloon Number: \(myBalloons[randomIndex].number)"
+        
+    }
+    
+    func createBalloons() {
+        
+        for var i = 1; i < 100; i++ {
+            var newBalloon = RedBalloon()
+            var randomIndex = Int(arc4random_uniform(UInt32(4)))
+            
+            newBalloon.number = i
+            
+            println("\(randomIndex)")
+            switch randomIndex {
+            case 1:
+                newBalloon.image = UIImage(named: "RedBalloon1.jpg")
+            case 2:
+                newBalloon.image = UIImage(named: "RedBalloon2.jpg")
+            case 3:
+                newBalloon.image = UIImage(named: "RedBalloon3.jpg")
+            default:
+                newBalloon.image = UIImage(named: "RedBalloon4.jpg")
+            }
+            
+//            println("\(newBalloon.image) \(newBalloon.number)")
+            
+            myBalloons.append(newBalloon)
+        }
 
+        
+    }
+    
+    
+            
 }
 
+        
+        
